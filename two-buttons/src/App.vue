f00324992f26aa9171b38cd570ccfcb115b59b14
Danny Calise <dannyjcalise@gmail.com>
2021-05-05 16:24:29 -0400

    console logs a random number

--- two-buttons/src/App.vue
+++ two-buttons/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Buttons />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Buttons from './components/Buttons.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Buttons
   }
 }
 </script>
@@ -23,6 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  border: 2px solid silver;
+  /* display: flex;
+  flex-direction: column; */
+  /* margin-top: 60px; */
 }
 </style>
